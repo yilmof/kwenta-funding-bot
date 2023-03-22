e61ef24101da090677415723af72109e06c1cd47
oi <80005091+yilmof@users.noreply.github.com>
2023-03-22 22:22:45 +0100

    Refactor Docker build and container naming convention to use 'kwenta_funding_simulated' instead of 'kwenta_funding_bot' for consistency and clarity. Stop and remove the old container before running the update

--- update.sh
+++ update.sh
@@ -2,14 +2,14 @@
 
 git pull
 
-docker stop kwenta-funding-bot
+docker stop kwenta-funding-simulated
 
-docker rmi kwenta_funding_bot -f
+docker rmi kwenta_funding_simulated -f
 
-docker build -t kwenta_funding_bot -f Dockerfile .
+docker build -t kwenta_funding_simulated -f Dockerfile .
 
 docker rmi $(docker images -qa -f 'dangling=true')
 
-docker rm kwenta-funding-bot
+docker rm kwenta-funding-simulated
 
-docker run --restart unless-stopped --name kwenta-funding-bot -d --env-file ./.env kwenta_funding_bot
+docker run --name kwenta-funding-simulated -d --env-file ./.env kwenta_funding_simulated
